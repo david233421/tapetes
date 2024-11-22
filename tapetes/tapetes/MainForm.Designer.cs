747dc47c767ba01c8be76015bb972058c504ece2
david233421 <aald080507hgtlpva4@cbtis217.edu.mx>
2024-11-22 17:02:05 -0600

    tapetes

---
+++ tapetes/tapetes/MainForm.Designer.cs
@@ -0,0 +1,399 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: CC2-CP03
+ * Date: 21/11/2024
+ * Time: 02:10 p. m.
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+namespace tapetes
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Designer variable used to keep track of non-visual components.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+		
+		/// <summary>
+		/// Disposes resources used by the form.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing) {
+				if (components != null) {
+					components.Dispose();
+				}
+			}
+			base.Dispose(disposing);
+		}
+		
+		/// <summary>
+		/// This method is required for Windows Forms designer support.
+		/// Do not change the method contents inside the source code editor. The Forms designer might
+		/// not be able to load this method if it was changed manually.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.label1 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.label3 = new System.Windows.Forms.Label();
+			this.label4 = new System.Windows.Forms.Label();
+			this.label5 = new System.Windows.Forms.Label();
+			this.label6 = new System.Windows.Forms.Label();
+			this.label7 = new System.Windows.Forms.Label();
+			this.txtbul = new System.Windows.Forms.TextBox();
+			this.txtlipin = new System.Windows.Forms.TextBox();
+			this.txtfru = new System.Windows.Forms.TextBox();
+			this.txttam = new System.Windows.Forms.TextBox();
+			this.txtflo = new System.Windows.Forms.TextBox();
+			this.txtnumper = new System.Windows.Forms.TextBox();
+			this.txtagu = new System.Windows.Forms.TextBox();
+			this.btncal = new System.Windows.Forms.Button();
+			this.txtfi = new System.Windows.Forms.TextBox();
+			this.btncalfin = new System.Windows.Forms.Button();
+			this.txt1 = new System.Windows.Forms.TextBox();
+			this.txt2 = new System.Windows.Forms.TextBox();
+			this.txt3 = new System.Windows.Forms.TextBox();
+			this.txt4 = new System.Windows.Forms.TextBox();
+			this.txt5 = new System.Windows.Forms.TextBox();
+			this.txt6 = new System.Windows.Forms.TextBox();
+			this.txt7 = new System.Windows.Forms.TextBox();
+			this.txttiem = new System.Windows.Forms.TextBox();
+			this.txtmano = new System.Windows.Forms.TextBox();
+			this.label8 = new System.Windows.Forms.Label();
+			this.label9 = new System.Windows.Forms.Label();
+			this.txt9 = new System.Windows.Forms.TextBox();
+			this.txt8 = new System.Windows.Forms.TextBox();
+			this.SuspendLayout();
+			// 
+			// label1
+			// 
+			this.label1.Location = new System.Drawing.Point(38, 66);
+			this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(150, 35);
+			this.label1.TabIndex = 0;
+			this.label1.Text = "tamaño del area por metro:";
+			// 
+			// label2
+			// 
+			this.label2.Location = new System.Drawing.Point(39, 102);
+			this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(150, 35);
+			this.label2.TabIndex = 1;
+			this.label2.Text = "bultos de aserrin:";
+			// 
+			// label3
+			// 
+			this.label3.Location = new System.Drawing.Point(39, 137);
+			this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(150, 35);
+			this.label3.TabIndex = 2;
+			this.label3.Text = "litro de pintura:";
+			// 
+			// label4
+			// 
+			this.label4.Location = new System.Drawing.Point(39, 172);
+			this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+			this.label4.Name = "label4";
+			this.label4.Size = new System.Drawing.Size(150, 35);
+			this.label4.TabIndex = 3;
+			this.label4.Text = "frutos secos:";
+			// 
+			// label5
+			// 
+			this.label5.Location = new System.Drawing.Point(38, 208);
+			this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+			this.label5.Name = "label5";
+			this.label5.Size = new System.Drawing.Size(150, 35);
+			this.label5.TabIndex = 4;
+			this.label5.Text = "flores por dosena:";
+			// 
+			// label6
+			// 
+			this.label6.Location = new System.Drawing.Point(38, 243);
+			this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+			this.label6.Name = "label6";
+			this.label6.Size = new System.Drawing.Size(150, 35);
+			this.label6.TabIndex = 5;
+			this.label6.Text = "num de personas:";
+			// 
+			// label7
+			// 
+			this.label7.Location = new System.Drawing.Point(38, 278);
+			this.label7.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+			this.label7.Name = "label7";
+			this.label7.Size = new System.Drawing.Size(150, 35);
+			this.label7.TabIndex = 6;
+			this.label7.Text = "litro de agua";
+			// 
+			// txtbul
+			// 
+			this.txtbul.Location = new System.Drawing.Point(196, 97);
+			this.txtbul.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+			this.txtbul.Name = "txtbul";
+			this.txtbul.Size = new System.Drawing.Size(148, 26);
+			this.txtbul.TabIndex = 7;
+			// 
+			// txtlipin
+			// 
+			this.txtlipin.Location = new System.Drawing.Point(196, 132);
+			this.txtlipin.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+			this.txtlipin.Name = "txtlipin";
+			this.txtlipin.Size = new System.Drawing.Size(148, 26);
+			this.txtlipin.TabIndex = 8;
+			// 
+			// txtfru
+			// 
+			this.txtfru.Location = new System.Drawing.Point(196, 168);
+			this.txtfru.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+			this.txtfru.Name = "txtfru";
+			this.txtfru.Size = new System.Drawing.Size(148, 26);
+			this.txtfru.TabIndex = 9;
+			// 
+			// txttam
+			// 
+			this.txttam.Location = new System.Drawing.Point(196, 62);
+			this.txttam.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+			this.txttam.Name = "txttam";
+			this.txttam.Size = new System.Drawing.Size(148, 26);
+			this.txttam.TabIndex = 11;
+			// 
+			// txtflo
+			// 
+			this.txtflo.Location = new System.Drawing.Point(198, 203);
+			this.txtflo.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+			this.txtflo.Name = "txtflo";
+			this.txtflo.Size = new System.Drawing.Size(148, 26);
+			this.txtflo.TabIndex = 12;
+			// 
+			// txtnumper
+			// 
+			this.txtnumper.Location = new System.Drawing.Point(196, 238);
+			this.txtnumper.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+			this.txtnumper.Name = "txtnumper";
+			this.txtnumper.Size = new System.Drawing.Size(148, 26);
+			this.txtnumper.TabIndex = 13;
+			// 
+			// txtagu
+			// 
+			this.txtagu.Location = new System.Drawing.Point(198, 274);
+			this.txtagu.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+			this.txtagu.Name = "txtagu";
+			this.txtagu.Size = new System.Drawing.Size(148, 26);
+			this.txtagu.TabIndex = 14;
+			// 
+			// btncal
+			// 
+			this.btncal.Location = new System.Drawing.Point(212, 412);
+			this.btncal.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+			this.btncal.Name = "btncal";
+			this.btncal.Size = new System.Drawing.Size(112, 35);
+			this.btncal.TabIndex = 15;
+			this.btncal.Text = "calcular";
+			this.btncal.UseVisualStyleBackColor = true;
+			this.btncal.Click += new System.EventHandler(this.BtncalClick);
+			// 
+			// txtfi
+			// 
+			this.txtfi.Location = new System.Drawing.Point(444, 412);
+			this.txtfi.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+			this.txtfi.Name = "txtfi";
+			this.txtfi.Size = new System.Drawing.Size(148, 26);
+			this.txtfi.TabIndex = 16;
+			// 
+			// btncalfin
+			// 
+			this.btncalfin.Location = new System.Drawing.Point(466, 452);
+			this.btncalfin.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+			this.btncalfin.Name = "btncalfin";
+			this.btncalfin.Size = new System.Drawing.Size(112, 35);
+			this.btncalfin.TabIndex = 17;
+			this.btncalfin.Text = "calculo final";
+			this.btncalfin.UseVisualStyleBackColor = true;
+			this.btncalfin.Click += new System.EventHandler(this.BtncalfinClick);
+			// 
+			// txt1
+			// 
+			this.txt1.Location = new System.Drawing.Point(444, 62);
+			this.txt1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+			this.txt1.Name = "txt1";
+			this.txt1.Size = new System.Drawing.Size(148, 26);
+			this.txt1.TabIndex = 18;
+			// 
+			// txt2
+			// 
+			this.txt2.Location = new System.Drawing.Point(442, 97);
+			this.txt2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+			this.txt2.Name = "txt2";
+			this.txt2.Size = new System.Drawing.Size(148, 26);
+			this.txt2.TabIndex = 19;
+			// 
+			// txt3
+			// 
+			this.txt3.Location = new System.Drawing.Point(444, 132);
+			this.txt3.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+			this.txt3.Name = "txt3";
+			this.txt3.Size = new System.Drawing.Size(148, 26);
+			this.txt3.TabIndex = 20;
+			// 
+			// txt4
+			// 
+			this.txt4.Location = new System.Drawing.Point(444, 168);
+			this.txt4.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+			this.txt4.Name = "txt4";
+			this.txt4.Size = new System.Drawing.Size(148, 26);
+			this.txt4.TabIndex = 21;
+			// 
+			// txt5
+			// 
+			this.txt5.Location = new System.Drawing.Point(444, 203);
+			this.txt5.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+			this.txt5.Name = "txt5";
+			this.txt5.Size = new System.Drawing.Size(148, 26);
+			this.txt5.TabIndex = 22;
+			// 
+			// txt6
+			// 
+			this.txt6.Location = new System.Drawing.Point(444, 238);
+			this.txt6.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+			this.txt6.Name = "txt6";
+			this.txt6.Size = new System.Drawing.Size(148, 26);
+			this.txt6.TabIndex = 23;
+			// 
+			// txt7
+			// 
+			this.txt7.Location = new System.Drawing.Point(444, 274);
+			this.txt7.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+			this.txt7.Name = "txt7";
+			this.txt7.Size = new System.Drawing.Size(148, 26);
+			this.txt7.TabIndex = 24;
+			// 
+			// txttiem
+			// 
+			this.txttiem.Location = new System.Drawing.Point(196, 309);
+			this.txttiem.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+			this.txttiem.Name = "txttiem";
+			this.txttiem.Size = new System.Drawing.Size(148, 26);
+			this.txttiem.TabIndex = 25;
+			// 
+			// txtmano
+			// 
+			this.txtmano.Location = new System.Drawing.Point(196, 345);
+			this.txtmano.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+			this.txtmano.Name = "txtmano";
+			this.txtmano.Size = new System.Drawing.Size(148, 26);
+			this.txtmano.TabIndex = 26;
+			// 
+			// label8
+			// 
+			this.label8.Location = new System.Drawing.Point(39, 314);
+			this.label8.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+			this.label8.Name = "label8";
+			this.label8.Size = new System.Drawing.Size(150, 35);
+			this.label8.TabIndex = 27;
+			this.label8.Text = "tiempo de diseño:";
+			// 
+			// label9
+			// 
+			this.label9.Location = new System.Drawing.Point(39, 349);
+			this.label9.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+			this.label9.Name = "label9";
+			this.label9.Size = new System.Drawing.Size(150, 35);
+			this.label9.TabIndex = 28;
+			this.label9.Text = "mano de obra:";
+			// 
+			// txt9
+			// 
+			this.txt9.Location = new System.Drawing.Point(442, 345);
+			this.txt9.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+			this.txt9.Name = "txt9";
+			this.txt9.Size = new System.Drawing.Size(148, 26);
+			this.txt9.TabIndex = 29;
+			// 
+			// txt8
+			// 
+			this.txt8.Location = new System.Drawing.Point(442, 309);
+			this.txt8.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+			this.txt8.Name = "txt8";
+			this.txt8.Size = new System.Drawing.Size(148, 26);
+			this.txt8.TabIndex = 30;
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(620, 498);
+			this.Controls.Add(this.txt8);
+			this.Controls.Add(this.txt9);
+			this.Controls.Add(this.label9);
+			this.Controls.Add(this.label8);
+			this.Controls.Add(this.txtmano);
+			this.Controls.Add(this.txttiem);
+			this.Controls.Add(this.txt7);
+			this.Controls.Add(this.txt6);
+			this.Controls.Add(this.txt5);
+			this.Controls.Add(this.txt4);
+			this.Controls.Add(this.txt3);
+			this.Controls.Add(this.txt2);
+			this.Controls.Add(this.txt1);
+			this.Controls.Add(this.btncalfin);
+			this.Controls.Add(this.txtfi);
+			this.Controls.Add(this.btncal);
+			this.Controls.Add(this.txtagu);
+			this.Controls.Add(this.txtnumper);
+			this.Controls.Add(this.txtflo);
+			this.Controls.Add(this.txttam);
+			this.Controls.Add(this.txtfru);
+			this.Controls.Add(this.txtlipin);
+			this.Controls.Add(this.txtbul);
+			this.Controls.Add(this.label7);
+			this.Controls.Add(this.label6);
+			this.Controls.Add(this.label5);
+			this.Controls.Add(this.label4);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.label1);
+			this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+			this.Name = "MainForm";
+			this.Text = "tapetes";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+		private System.Windows.Forms.TextBox txt8;
+		private System.Windows.Forms.TextBox txt9;
+		private System.Windows.Forms.Label label9;
+		private System.Windows.Forms.Label label8;
+		private System.Windows.Forms.TextBox txtmano;
+		private System.Windows.Forms.TextBox txttiem;
+		private System.Windows.Forms.TextBox txt7;
+		private System.Windows.Forms.TextBox txt6;
+		private System.Windows.Forms.TextBox txt5;
+		private System.Windows.Forms.TextBox txt4;
+		private System.Windows.Forms.TextBox txt3;
+		private System.Windows.Forms.TextBox txt2;
+		private System.Windows.Forms.TextBox txt1;
+		private System.Windows.Forms.Button btncalfin;
+		private System.Windows.Forms.TextBox txtfi;
+		private System.Windows.Forms.Button btncal;
+		private System.Windows.Forms.TextBox txtagu;
+		private System.Windows.Forms.TextBox txtnumper;
+		private System.Windows.Forms.TextBox txtflo;
+		private System.Windows.Forms.TextBox txttam;
+		private System.Windows.Forms.TextBox txtfru;
+		private System.Windows.Forms.TextBox txtlipin;
+		private System.Windows.Forms.TextBox txtbul;
+		private System.Windows.Forms.Label label7;
+		private System.Windows.Forms.Label label6;
+		private System.Windows.Forms.Label label5;
+		private System.Windows.Forms.Label label4;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Label label1;
+	}
+}
---
+++ tapetes/tapetes/MainForm.cs
@@ -0,0 +1,228 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: CC2-CP03
+ * Date: 21/11/2024
+ * Time: 02:10 p. m.
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace tapetes
+{
+	/// <summary>
+	/// Description of MainForm.
+	/// </summary>
+	public partial class MainForm : Form
+	{
+		public MainForm()
+		{
+			//
+			// The InitializeComponent() call is required for Windows Forms designer support.
+			//
+			InitializeComponent();
+			
+			//
+			// TODO: Add constructor code after the InitializeComponent() call.
+			//
+		}
+		
+		void BtncalClick(object sender, EventArgs e)
+		{
+			string tam;
+		    int tamNum, tamResultado;
+		    tam = txttam.Text;
+		    if (int.TryParse(tam, out tamNum))
+		    {
+		        tamResultado = tamNum * 100;
+		        txt1.Text = tamResultado.ToString();
+		    }
+		    else
+		    {
+		        txt1.Text = "Error: Ingrese un número válido.";
+		    }
+
+			try
+            {
+
+                int cantidad = int.Parse(txtbul.Text);
+                int kilos = cantidad * 10;    
+
+
+                double precioPorKilo = 19.50;
+                double presupuesto = kilos * precioPorKilo;
+
+
+                txt2.Text = presupuesto.ToString("F2");
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("Por favor, ingresa un número válido en el campo de cantidad.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }	
+	
+			try
+            {
+
+                int cantidad = int.Parse(txtlipin.Text); 
+                int litros = cantidad * 5;      
+
+                double precioPorLitro = 150.0;
+                double presupuesto = litros * precioPorLitro;
+
+
+                txt3.Text = presupuesto.ToString("F2"); // Formateado con 2 decimales
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("Por favor, ingresa un número válido en el campo de cantidad.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }  
+
+			try
+            {
+
+                int cantidad = int.Parse(txtfru.Text); 
+
+
+                double precioPorKilo = 120.0;
+
+
+                double costoTotal = cantidad * precioPorKilo;
+
+
+                txt4.Text = costoTotal.ToString("F2"); 
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("Por favor, ingresa un número válido en el campo de cantidad.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }   
+
+
+			try
+            {
+
+                int cantidad = int.Parse(txtflo.Text); 
+
+                double precioPorDocena = 200.0;
+
+
+                double costoTotal = cantidad * precioPorDocena;
+
+
+                txt5.Text = costoTotal.ToString("F2"); 
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("Por favor, ingresa un número válido en el campo de cantidad.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }   
+
+
+			  try
+            {
+
+                int cantidadPersonas = int.Parse(txtnumper.Text); 
+
+
+                double pagoPorPersona = 200.0;
+
+
+                double pagoTotal = cantidadPersonas * pagoPorPersona;
+
+
+                txt6.Text = pagoTotal.ToString("F2"); 
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("Por favor, ingresa un número válido en el campo de cantidad.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }   
+
+
+			 try
+            {
+                // Leer la cantidad ingresada en txtagu
+                int cantidadLitros = int.Parse(txtagu.Text); // Cantidad de litros ingresada
+
+
+                double precioPorLitro = 12.0;
+
+
+                double costoTotal = cantidadLitros * precioPorLitro;
+
+
+                txt7.Text = costoTotal.ToString("F2");
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("Por favor, ingresa un número válido en el campo de cantidad.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            } 
+
+
+			try
+            {
+
+                int cantidadHoras = int.Parse(txttiem.Text); 
+
+                double pagoPorHora = 50.0;
+
+
+                double costoTotal = cantidadHoras * pagoPorHora;
+
+
+                txt8.Text = costoTotal.ToString("F2");
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("Por favor, ingresa un número válido en el campo de cantidad.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            
+            
+            try
+            {
+                // Leer la cantidad ingresada en txtmano
+                int cantidadPersonas = int.Parse(txtmano.Text); // Cantidad de personas ingresada
+
+                // Pago por persona
+                double pagoPorPersona = 180.0;
+
+                // Calcular el costo total
+                double costoTotal = cantidadPersonas * pagoPorPersona;
+
+                // Mostrar el resultado en txt9
+                txt9.Text = costoTotal.ToString("F2"); // Formato con 2 decimales
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("Por favor, ingresa un número válido en el campo de cantidad.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+           
+		}
+		void BtncalfinClick(object sender, EventArgs e)
+		{
+			try
+            {
+                // Leer los valores de los campos
+                double valor1 = double.Parse(txt1.Text);
+                double valor2 = double.Parse(txt2.Text);
+                double valor3 = double.Parse(txt3.Text);
+                double valor4 = double.Parse(txt4.Text);
+                double valor5 = double.Parse(txt5.Text);
+                double valor6 = double.Parse(txt6.Text);
+                double valor7 = double.Parse(txt7.Text);
+                double valor8 = double.Parse(txt8.Text);
+                double valor9 = double.Parse(txt9.Text);
+
+                // Sumar todos los valores
+                double sumaTotal = valor1 + valor2 + valor3 + valor4 + valor5 + valor6 + valor7 + valor8 + valor9;
+
+                // Mostrar el resultado en txtfi
+                txtfi.Text = sumaTotal.ToString("F2"); // Formato con 2 decimales
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("Por favor, asegúrate de que todos los campos tengan valores numéricos.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+		}
+	}
+}
